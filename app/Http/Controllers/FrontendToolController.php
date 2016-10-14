339b86290e252f75963aed46f7a3ec1fcf2d5b94
Kevin Burek <git.commit@kb0.org>
2016-10-13 22:18:02 -0400

    Faker is here, but not working completely yet.

--- app/Http/Controllers/FrontendToolController.php
+++ app/Http/Controllers/FrontendToolController.php
@@ -7,14 +7,30 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Kb0\FrontendBuddy\UserStub;
+use fzaninotto\faker as faker;
 
 class FrontendToolController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    private $personFaker;
+
+    function getRandomUserStubs($num) {
+        $users = array($num);
+        for ($i = 0; $i <= $num; $i++) {
+            $u = new UserStub();
+            $u->firstName = $this->personFaker->firstName();
+            $u->lastName = $this->personFaker->lastName();
+            $u->userName = "user".$i;
+            $users[$i] =  $u;
+        }
+        return $users;
+    }
+
     function index() {
         
-        $user = new UserStub();
+        $user = $this->getRandomUserStubs(1)[0];
+        $personFaker = new faker\en_US\Person();
 
         return view('FrontendTools.index')
             ->with('desc', "Empty user!")
